0bf97aa03e333c6a42cc1adfc3f737282bc9329a
Matthew Sant-Miller <msantmiller@gmail.com>
2026-08-20 14:28:54 -0400

    Commit initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to the codebase for the Aerostat Studio website!
+
+This repository was initialized using the Vite React template.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <section>
+        <h1>Aerostat Studio</h1>
+      </section>
+    </>
+  );
+}
